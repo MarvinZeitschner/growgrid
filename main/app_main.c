92c335fce2549134aac15f638af6407e13c29cb7
MarvinZeitschner <m.zeitschner@gmail.com>
2025-07-22 17:29:33 +0200

    feat: init i2c

--- main/app_main.c
+++ main/app_main.c
@@ -1,8 +1,16 @@
+#include "board.h"
+#include "components/drivers/include/i2c.h"
+#include "driver/i2c_types.h"
+#include "esp_err.h"
 #include "freertos/FreeRTOS.h" // IWYU pragma: keep - required before other FreeRTOS headers
 #include "freertos/projdefs.h"
 #include "freertos/task.h"
+#include "hal/i2c_types.h"
 #include "rgb_led.h"
 #include <stdio.h>
+
+i2c_master_bus_handle_t i2c_mst_bus;
+i2c_master_dev_handle_t tsl2561_dev_handle;
 
 void hello_task(void *pvParameter) {
   while (1) {
@@ -15,6 +23,12 @@ void hello_task(void *pvParameter) {
 }
 
 void app_main(void) {
+  ESP_ERROR_CHECK(i2c_init(I2C_NUM_0, SDA_GPIO, SCL_GPIO, &i2c_mst_bus));
+
+  ESP_ERROR_CHECK(
+      i2c_add_device(i2c_mst_bus, 0x39, 100000, &tsl2561_dev_handle));
+
   rgb_led_init();
+
   xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
 }
--- main/components/board/include/board.h
+++ main/components/board/include/board.h
@@ -2,5 +2,7 @@
 #define BOARD_H
 
 #define LED_RGB_GPIO 8
+#define SDA_GPIO 6
+#define SCL_GPIO 7
 
 #endif
