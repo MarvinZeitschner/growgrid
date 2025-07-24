7951137ff6a8d7af4072aa81696135375b75db66
MarvinZeitschner <m.zeitschner@gmail.com>
2025-07-24 17:26:26 +0200

    feat: added bmp280 with derived driver from bme280

--- main/app_main.c
+++ main/app_main.c
@@ -1,4 +1,5 @@
 #include "board.h"
+#include "components/bmp280/include/bmp280.h"
 #include "components/tsl2561/include/tsl2561.h"
 #include "esp_err.h"
 #include "esp_log.h"
@@ -17,6 +18,7 @@ i2c_bus_handle_t i2c_bus;
 i2c_bus_device_handle_t tsl2561_dev_handle;
 
 tsl2561_handle_t tsl2561;
+bmp280_handle_t bmp280;
 
 void read_sensors_task(void *pvParameter) {
   while (1) {
@@ -28,7 +30,15 @@ void read_sensors_task(void *pvParameter) {
       ESP_LOGE(TAG, "Failed to read TSL2561: %s", esp_err_to_name(err));
     }
 
-    vTaskDelay(pdMS_TO_TICKS(1000));
+    float temperature = 0.0;
+    err = bmp280_read_temperature(bmp280, &temperature);
+    if (err == ESP_OK) {
+      printf("Temperature: %f deg\n", temperature);
+    } else {
+      ESP_LOGE(TAG, "Failed to read bmp280 temp: %s\n", esp_err_to_name(err));
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(2000));
   }
 }
 
@@ -56,10 +66,20 @@ void tsl2561_configure() {
       tsl2561_init(&tsl2561, tsl2561_dev_handle, &tsl2561_conf, 1000));
 }
 
+void bmp280_configure() {
+  bmp280 = bmp280_create(i2c_bus, BMP280_I2C_ADDRESS_DEFAULT);
+  esp_err_t err = bmp280_default_init(bmp280);
+  ESP_LOGI(TAG, "bmp280_default_init: %s", esp_err_to_name(err));
+}
+
 void app_main(void) {
   i2c_configure();
   tsl2561_configure();
+  bmp280_configure();
   rgb_led_init();
+
+  // Wait for sensors to become stable (esp. bmp280)
+  vTaskDelay(pdMS_TO_TICKS(100));
 
   xTaskCreate(&read_sensors_task, "read_sensors_task", 4096, NULL, 5, NULL);
 }
